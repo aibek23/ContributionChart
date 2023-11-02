63f56a1a7df96a7731eeb169fc59c56b5bd66ec0
kalygul <aibekasanakunuulu@gmail.com>
2023-11-03 00:54:10 +0600

    day_discription

--- src/ContributionChart.js
+++ src/ContributionChart.js
@@ -1,6 +1,6 @@
-import React, { useState,useEffect , useRef } from "react";
-import { format, parseISO } from 'date-fns';
-import { ru } from 'date-fns/locale';
+import React, { useState, useEffect, useRef } from "react";
+import { format, parseISO } from "date-fns";
+import { ru } from "date-fns/locale";
 const ContributionChart = ({ data }) => {
   const rows = 7;
   const columns = 51;
@@ -27,22 +27,21 @@ const ContributionChart = ({ data }) => {
 
   useEffect(() => {
     // Добавьте обработчик события для всего документа при монтировании компонента.
-    document.addEventListener('click', handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
 
     // Удалите обработчик события при размонтировании компонента.
     return () => {
-      document.removeEventListener('click', handleClickOutside);
+      document.removeEventListener("click", handleClickOutside);
     };
   }, []);
 
   const handleClickOutside = (e) => {
     if (blockRef.current && !blockRef.current.contains(e.target)) {
-        setHoveredDate(null)
-        setHoveredCount(null)
+      setHoveredDate(null);
+      setHoveredCount(null);
     }
   };
 
-  
   const generateColor = (count) => {
     if (count === 0) {
       return "#EDEDED";
@@ -56,7 +55,6 @@ const ContributionChart = ({ data }) => {
       return "#254E77";
     }
   };
-
 
   const generateChart = () => {
     const chart = [];
@@ -78,8 +76,7 @@ const ContributionChart = ({ data }) => {
             style={{
               backgroundColor: color,
             }}
-
-            onClick={(e) => handleCellClick(dateString, e , count)}
+            onClick={(e) => handleCellClick(dateString, e, count)}
           ></div>
         );
 
@@ -95,15 +92,17 @@ const ContributionChart = ({ data }) => {
 
     return chart;
   };
-  const handleCellClick = (dateString, event,count) => {
-    setHoveredDate(dateString);
-    setHoveredCount(count)
-    setTooltipPosition({ x: event.clientX, y: event.clientY });
+  const handleCellClick = (dateString, event, count) => {
+    if (count !== null) {
+      setHoveredDate(dateString);
+      setHoveredCount(count);
+      setTooltipPosition({ x: event.clientX, y: event.clientY });
+    }
   };
- 
+
   return (
     <div className="container">
-      <div className="contribution" ref={blockRef}  >
+      <div className="contribution" ref={blockRef}>
         <div style={{ display: "flex" }}>
           <div className="weekdays">
             {weekdays.map((day, index) => (
@@ -137,18 +136,63 @@ const ContributionChart = ({ data }) => {
             </div>
           </div>
         </div>
-        
-        {hoveredDate && (
+
+        <div className="day_discription">
+          <p className="Less_More">Меньше</p>
+          <div
+            className="day"
+            style={{
+              backgroundColor: "#EDEDED",
+            }}
+            onClick={(e) => handleCellClick("", e, 0)}
+          ></div>
+          <div
+            className="day"
+            style={{
+              backgroundColor: "#ACD5F2",
+            }}
+            onClick={(e) => handleCellClick("", e, "1-9")}
+          ></div>
+             <div
+            className="day"
+            style={{
+              backgroundColor: "#7FA8C9",
+            }}
+            onClick={(e) => handleCellClick("", e, "10-19")}
+          ></div>
+            <div
+            className="day"
+            style={{
+              backgroundColor: "#527BA0",
+            }}
+            onClick={(e) => handleCellClick("", e, "20-29")}
+          ></div>
+             <div
+            className="day"
+            style={{
+              backgroundColor: "#254E77",
+            }}
+            onClick={(e) => handleCellClick("", e, "30+")}
+          ></div>
+                   <p className="Less_More">Больше</p>
+        </div>
+        {hoveredCount !== null && (
           <div
             className="tooltip"
-            style={{ 
-    
-                transform: 'translateX(-50%) translateY(-100%)', top: tooltipPosition.y-20 , left: tooltipPosition.x}}
+            style={{
+              transform: "translateX(-50%) translateY(-100%)",
+              top: tooltipPosition.y - 20,
+              left: tooltipPosition.x,
+            }}
           >
-            <p>{hoveredCount} contributions </p>
-            <p style={{color: "#7C7C7C"}}>{ format(parseISO(hoveredDate), 'EEEE, MMMM d, yyyy',{ locale: ru })} </p>
-            
-
+            <p>{hoveredCount===0?"No":hoveredCount} contributions </p>
+            {hoveredDate && (
+              <p style={{ color: "#7C7C7C" }}>
+                {format(parseISO(hoveredDate), "EEEE, MMMM d, yyyy", {
+                  locale: ru,
+                })}{" "}
+              </p>
+            )}
           </div>
         )}
       </div>
@@ -157,6 +201,3 @@ const ContributionChart = ({ data }) => {
 };
 
 export default ContributionChart;
-
-
-
